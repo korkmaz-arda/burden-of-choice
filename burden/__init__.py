8f46583b928ab6427269ad9272ca5f91f4d7edd5
korkmaz-arda <arda.korkmaz1996@gmail.com>
2023-07-31 18:49:32 +0300

    Add memory.py and burden.py

---
+++ burden/__init__.py
@@ -0,0 +1,4 @@
+# BURDEN OF CHOICE
+
+__app_name__ = "burden"
+__version__ = "0.1.0"
---
+++ burden/memory.py
@@ -0,0 +1,42 @@
+import json
+
+
+class MemoryFile:
+    _instance = None
+    history = []
+
+    def __new__(cls, memory_path=None, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls.memory_path = memory_path if memory_path is not None else "decision_memory.json"
+        return cls._instance
+
+    def load(self):
+        try:
+            with open(self.path, "r") as file:
+                return MemoryResponse(data=data)
+        except FileNotFoundError:
+            msg = "Error: Unable to access memory file."
+            return MemoryResponse(msg)
+        except json.JSONDecodeError:
+            msg = "Error: Invalid JSON format in the memory file."
+            return MemoryResponse(msg)
+        except Exception as e:
+            msg = f"Error: An unexpected error occurred while loading memory: {e}"
+            return MemoryResponse(msg)
+
+    def save(self, memory):
+        history.append(memory.copy())
+        try:
+            with open(self.path, "w") as file:
+                json.dump(memory, file)
+            return MemoryResponse()
+        except Exception as e:
+            msg = f"Error: An unexpected error occurred while saving memory: {e}"
+            return MemoryResponse(msg)
+
+
+class MemoryResponse:
+    def __init__(self, error_message=None, data=None):
+        self.error_message = error_message
+        self.data = data
