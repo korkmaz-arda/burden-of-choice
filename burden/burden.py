68cfadbbfbc9aeb63fbd20eebe23856d9633729a
korkmaz-arda <arda.korkmaz1996@gmail.com>
2023-08-02 12:36:00 +0300

    Remove validation functions

--- burden/burden.py
+++ burden/burden.py
@@ -11,30 +11,15 @@ def load():
     if response.error_message:
         typer.echo(response.error_message)
         raise typer.Exit()
-    
+
     return response.data
+
 
 def save(data):
     memory = MemoryFile()
     response = memory.save(data)
     if response.error_message:
         typer.echo(response.error_message)
-
-
-def validate_category(category):
-    data = load()
-    if category not in data:
-        typer.echo(f"Error: Category '{category}' does not exist.")
-        raise typer.Abort()
-
-
-def validate_option(option, category):
-    validate_category(category)
-
-    data = load()
-    if option not in data[category]:
-        typer.echo(f"Error: Option '{option}' does not exist in '{category}' category.")
-        raise typer.Abort()
 
 
 if __name__ == "__main__":
